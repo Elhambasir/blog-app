6fcc70a1a3f6f4c1e484b35e07a3fd05a2a0d786
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-19 15:03:56 +0430

    Update 20230718151106_add_post_ref_to_likes.rb
    Add index to table likes on post_id

--- db/migrate/20230718151106_add_post_ref_to_likes.rb
+++ db/migrate/20230718151106_add_post_ref_to_likes.rb
@@ -1,5 +1,6 @@
 class AddPostRefToLikes < ActiveRecord::Migration[7.0]
   def change
     add_reference :likes, :post, null: false, foreign_key: true
+    add_index :likes, :post_id, name: 'index_likes_on_post_id'
   end
 end
