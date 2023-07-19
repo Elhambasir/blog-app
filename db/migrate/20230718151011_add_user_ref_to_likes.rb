a2b6389cce5d3e7e8968db5051a1cd35a6a62170
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-19 15:03:31 +0430

    Update 20230718151011_add_user_ref_to_likes.rb
    add ref to like table

--- db/migrate/20230718151011_add_user_ref_to_likes.rb
+++ db/migrate/20230718151011_add_user_ref_to_likes.rb
@@ -1,5 +1,6 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, null: false, foreign_key: true
+    add_reference :likes, :author, foreign_key: { to_table: 'users' }
+    add_index :likes, :author_id, name: 'index_likes_on_author_id'
   end
 end
