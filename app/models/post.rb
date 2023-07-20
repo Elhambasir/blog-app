99766c2c167cdf88afdc97d23bb0d4067a699280
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 10:32:17 +0430

    Update post.rb
    Remove populated data from the application
    Use method to update the counter instead of counter_cache

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,27 +1,18 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', counter_cache: :post_counter
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: :post_id
+  has_many :likes, foreign_key: :post_id
   has_many :users, through: :likes
 
-  first_user = User.find_by(name: 'Tom')
-
-  Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
-  Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
-  Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
-  Post.create(author: first_user, title: 'Hello', text: 'This is my fourth post')
-
-  Post.fine(1).delete
+  after_save :update_post_counter
 
   # A method that updates the posts counter for a user.
-  def self.update_post_counter(user_id)
-    user = User.find(user_id)
-    user.update(post_counter: user.posts.count)
+  def update_post_counter
+    author.increment!(:post_counter)
   end
 
   # A method which returns the 5 most recent comments for a given post.
-  def recent_comments(post_id)
-    post = Post.find(post_id)
-    post.comments.order(created_at: :desc).limit(5)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
