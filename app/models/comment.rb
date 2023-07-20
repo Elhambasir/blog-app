1e5f793a6aeb2e647e79d486e63e8ad7719d8e5d
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 10:30:29 +0430

    Update comment.rb
    Remove populated data from the application

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,22 +1,11 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post, counter_cache: :comment_counter
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
   validates :text, presence: true
 
-  first_post = Post.find(1)
-  first_user = User.find_by(name: 'Tom')
-  second_user = User.find(5)
-
-  Comment.create(post: first_post, author: second_user, text: 'Hi Tom!')
-  Comment.create(post: first_post, user: second_user, text: 'Your post is nice!')
-  Comment.create(post: first_post, user: first_user, text: 'Perfect')
-  Comment.create(post: first_post, user: first_user, text: 'I liked your post')
-  Comment.create(post: first_post, user: second_user, text: 'I love your post')
-  Comment.create(post: first_post, user: first_user, text: 'You look great!')
-
+  after_save :update_comment_counter
   # A method that updates the comments counter for a post.
-  def self.update_comment_counter(post_id)
-    post = Post.find(post_id)
-    post.update(comment_counter: post.comments.count)
+  def update_comment_counter
+    post.increment!(:comment_counter)
   end
 end
