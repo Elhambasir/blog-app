c1f8cb5d7d854f55ecf9419b04c27578c9486f98
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 10:34:29 +0430

    Update 20230718142009_add_user_ref_to_comments.rb

--- db/migrate/20230718142009_add_user_ref_to_comments.rb
+++ db/migrate/20230718142009_add_user_ref_to_comments.rb
@@ -1,6 +1,5 @@
 class AddUserRefToComments < ActiveRecord::Migration[7.0]
   def change
     add_reference :comments, :author, foreign_key: { to_table: 'users' } 
-    add_index :comments, :author_id, name: 'index_comments_on_author_id'
   end
 end
