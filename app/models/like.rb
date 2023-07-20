2d7afd4f2cc97e059467650f1b5617612482788a
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 10:35:17 +0430

    Update like.rb
    Use method to update counter instead of counter_cache

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,11 @@
 class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post, counter_cache: :like_counter
+  belongs_to :user, class_name: 'User'
+  belongs_to :post
+
+  after_save :update_like_counter
 
   # A method that updates the likes counter for a post.
-  def self.update_like_counter(post_id)
-    post = Post.find(post_id)
-    post.update(like_counter: post.likes.count)
+  def update_like_counter
+    post.increment!(:like_counter)
   end
 end
