ec2f11654326639286a4698dddbef831d2d0f907
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 10:34:06 +0430

    Update user.rb
    Remove populated data from the application.
    Indicate the column that holds this association in the respective models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,13 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
   has_many :liked_posts, through: :likes, source: :post
   validates :name, presence: true
   validates :photo, presence: true
   validates :bio, presence: true
 
-  User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-  User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-              bio: 'Teacher from Poland.')
-
-  first_user = User.find_by(name: 'Tom')
-  first_user.name = 'Elham'
-  first_user.save
-
-  def recent_posts(author_id)
-    author = User.find(author_id)
-    author.posts.order(created_at: :desc).limit(5)
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
   end
 end
