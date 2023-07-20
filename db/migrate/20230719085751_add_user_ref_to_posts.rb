4064a93346ddb04fc975c69911aa1c409d8f90b8
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-20 09:20:59 +0430

    Fix user_id and author_id issue

--- db/migrate/20230719085751_add_user_ref_to_posts.rb
+++ db/migrate/20230719085751_add_user_ref_to_posts.rb
@@ -1,6 +1,5 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
     add_reference :posts, :author, foreign_key: { to_table: 'users' } 
-    add_index :posts, :author_id, name: 'index_posts_on_author_id'
   end
 end
