91ba52ee109cbc64f2ca92e4fdbf55ad5c951852
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-25 21:29:52 +0430

    Update routes.rb
    Add required routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  post '/users/new', to: 'users#new'
+  post '/users/edit/:id', to: 'users#edit'
+  post '/users/:id/posts/edit/:id', to: 'posts#edit'
+  post '/users/:id/posts/new', to: 'posts#new'
+  post '/users/:id/posts/:id/comments/new', to: 'comments#new'
+  post '/users/:id/posts/:id/comments/edit/:id', to: 'comments#edit'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:index, :show]
+    end
   end
 end
