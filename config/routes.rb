f609922be88573e24b79ad7cdf8290f5bea7239c
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-26 22:34:30 +0430

    Update routes.rb
    Change id to user_id in routes to make it more descriptive

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   post '/users/new', to: 'users#new'
-  post '/users/edit/:id', to: 'users#edit'
-  post '/users/:id/posts/edit/:id', to: 'posts#edit'
-  post '/users/:id/posts/new', to: 'posts#new'
-  post '/users/:id/posts/:id/comments/new', to: 'comments#new'
-  post '/users/:id/posts/:id/comments/edit/:id', to: 'comments#edit'
+  post '/users/edit/:user_id', to: 'users#edit'
+  post '/users/:user_id/posts/edit/:post_id', to: 'posts#edit'
+  post '/users/:user_id/posts/new', to: 'posts#new'
+  post '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new'
+  post '/users/:user_id/posts/:post_id/comments/edit/:comment_id', to: 'comments#edit'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
       resources :comments, only: [:index, :show]
