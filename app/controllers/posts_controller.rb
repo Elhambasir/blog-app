8f7fac5efee1777b92032e5f298dfddd88235bab
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-28 14:13:15 +0430

    Update posts_controller.rb
    Add action create
    Add method find_user
    Add method post_params
    Add before_action property

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,41 @@
 class PostsController < ApplicationController
+  
+  before_action :find_user, only: %i[index show new edit create]
   def index
-    @user_id = params[:user_id]
-    @user = User.find(@user_id)
-    @posts = Post.where(author_id: @user_id)
+    @posts = @user.posts.order(created_at: :asc)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post =  @user.posts.find(params[:id])
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
   def edit; end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      redirect_to user_posts_path(current_user), notice: 'Post created successfully'
+    else
+      flash.now[:alert] = @post.errors.full_messages.first if @post.errors.any?
+      render :new, status: 400
+    end
+  end
+
+  private
+  
+  def find_user
+    # @user = User.find(params[:user_id])
+    @user = User.includes(:posts, posts: [:comments, { comments: [:author] }]).find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+  
 end
