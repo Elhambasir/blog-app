d7c08e6c5637dea11fc2496b2558bdb7f3eb1003
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-28 14:18:13 +0430

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  post '/users/new', to: 'users#new'
-  post '/users/edit/:user_id', to: 'users#edit'
-  post '/users/:user_id/posts/edit/:post_id', to: 'posts#edit'
-  post '/users/:user_id/posts/new', to: 'posts#new'
-  post '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new'
-  post '/users/:user_id/posts/:post_id/comments/edit/:comment_id', to: 'comments#edit'
+  get '/users/:user_id/posts/:id/likes', to: 'likes#create', as: 'user_post_likes'
   root to: "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      resources :comments, only: [:index, :show]
+  resources :users, only: [:index, :show, :new] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new,:index, :show, :create]
+      resources :likes, only: [:create]
     end
   end
 end
