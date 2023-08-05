132cfd6b9711ec3fdd38fdaf75faca712c945816
Elhambasir <elham1378basir@gmail.com>
2023-08-05 12:43:34 +0430

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: :post_id
-  has_many :likes, foreign_key: :post_id
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :users, through: :likes
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   after_save :update_post_counter
+  after_destroy :update_post_counter
   # A method that updates the posts counter for a user.
   def update_post_counter
-    author.increment!(:posts_counter)
+    author.update(posts_counter: author.posts.count)
   end
 
   # A method which returns the 5 most recent comments for a given post.
