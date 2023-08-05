05aa9ff10ff58dbad717b291fb39bedbf44ee8e1
Elhambasir <elham1378basir@gmail.com>
2023-08-05 17:35:10 +0430

    Update api_comments_controller.rb
    Add create method to get data from params and create new comment

--- app/controllers/api_comments_controller.rb
+++ app/controllers/api_comments_controller.rb
@@ -1,8 +1,24 @@
 class ApiCommentsController < ActionController::API
+  before_action :set_post, only: %i[new create update]
   def index
     @user = User.find(params[:user_id])
-    @post = Post.where(author: @user).find(params[:post_id])
+    @post = @user.posts.find(params[:post_id])
     @comments = @post.comments
     render json: @comments
+  end
+
+  def create
+    @comment = Comment.new(author_id: params[:user_id].to_i, post_id: params[:post_id].to_i, text: params[:text])
+    if @comment.save
+      render json: @comment, status: 201
+    else
+      render json: { errors: @comment.errors.full_messages }, status: 400
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
